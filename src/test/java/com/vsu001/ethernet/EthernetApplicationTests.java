6812c8143705ade47b2a090ca11fc26481608a1e
suvoonhou <voonhousu@gmail.com>
2020-12-30 16:48:30 +0800

    Initialise Spring Boot project

---
+++ src/test/java/com/vsu001/ethernet/EthernetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsu001.ethernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
