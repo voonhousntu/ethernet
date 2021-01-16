c85867cc47e41cebd35741937e1330353f2b9c90
suvoonhou <voonhousu@gmail.com>
2021-01-16 15:17:00 +0800

    Implemented remaining TODOs in CoreServiceImpl

--- core/src/main/java/com/vsu001/ethernet/core/service/CoreServiceImpl.java
+++ core/src/main/java/com/vsu001/ethernet/core/service/CoreServiceImpl.java
@@ -15,14 +15,48 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
 
   private final JdbcTemplate jdbcTemplate;
+  private final BlocksServiceImpl blocksService;
   private final BlockTsMappingServiceImpl blockTsMappingService;
+  private final ContractsServiceImpl contractsService;
+  private final LogsServiceImpl logsService;
+  private final TokensServiceImpl tokensService;
+  private final TokenTransfersServiceImpl tokenTransfersService;
+  private final TracesServiceImpl tracesService;
+  private final TransactionsServiceImpl transactionsService;
 
   public CoreServiceImpl(
       JdbcTemplate jdbcTemplate,
-      BlockTsMappingServiceImpl blockTsMappingService
+      BlocksServiceImpl blocksService,
+      BlockTsMappingServiceImpl blockTsMappingService,
+      ContractsServiceImpl contractsService,
+      LogsServiceImpl logsService,
+      TokensServiceImpl tokensService,
+      TokenTransfersServiceImpl tokenTransfersService,
+      TracesServiceImpl tracesService,
+      TransactionsServiceImpl transactionsService
   ) {
     this.jdbcTemplate = jdbcTemplate;
+    this.blocksService = blocksService;
     this.blockTsMappingService = blockTsMappingService;
+    this.contractsService = contractsService;
+    this.logsService = logsService;
+    this.tokensService = tokensService;
+    this.tokenTransfersService = tokenTransfersService;
+    this.tracesService = tracesService;
+    this.transactionsService = transactionsService;
+  }
+
+  private static UpdateResponse buildUpdateResponse() {
+    Instant time = Instant.now();
+    return UpdateResponse.newBuilder()
+        .setNumber(-1L)
+        .setTimestamp(
+            Timestamp.newBuilder()
+                .setSeconds(time.getEpochSecond())
+                .setNanos(time.getNano())
+                .build()
+        )
+        .build();
   }
 
   @Override
@@ -30,50 +64,15 @@ public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
       UpdateRequest request,
       StreamObserver<UpdateResponse> responseObserver
   ) {
-    log.info("Updating `block_timestamp_mapping`");
+    log.info("Updating `block_timestamp_mapping` table");
 
     try {
-      String outputPath = blockTsMappingService.getOutputPath();
-      String filename = blockTsMappingService.getFilename();
-      String struct = blockTsMappingService.getStructStr();
-      String tableName = blockTsMappingService.getTableName();
-      String tmpTableName = blockTsMappingService.getTmpTableName();
-      String schema = blockTsMappingService.getSchemaStr();
-
-      // Fetch results from BigQuery
-      TableResult tableResult = blockTsMappingService.fetchFromBq(request);
-
-      // Write query results to ORC file with random (UUID) filename in HDFS
-      writeTableResults(blockTsMappingService, tableResult);
-//      writeTableResults(outputPath, filename, struct, tableResult);
-
-      // Create temporary Hive table
-      createTmpTable(blockTsMappingService);
-//      createTmpTable(schema, tmpTableName, outputPath);
-
-      // Insert data from temporary Hive table
-      populateHiveTable(blockTsMappingService);
-//      populateHiveTable(tableName, tmpTableName);
-
-      // Remove temporary Hive table + temporary ORC file
-      dropTmpTable(blockTsMappingService);
-//      dropTmpTable(tmpTableName);
-
-      Instant time = Instant.now();
-      UpdateResponse reply = UpdateResponse.newBuilder()
-          .setNumber(-1L)
-          .setTimestamp(
-              Timestamp.newBuilder()
-                  .setSeconds(time.getEpochSecond())
-                  .setNanos(time.getNano())
-                  .build()
-          )
-          .build();
+      // Fetch and populate `block_timestamp_mapping` table
+      fetchAndPopulateHiveTable(blockTsMappingService, request);
 
       // Return response
-      responseObserver.onNext(reply);
+      responseObserver.onNext(buildUpdateResponse());
       responseObserver.onCompleted();
-
     } catch (InterruptedException | IOException e) {
       e.printStackTrace();
       responseObserver.onError(e.fillInStackTrace());
@@ -82,8 +81,19 @@ public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
 
   @Override
   public void updateBlocks(UpdateRequest request, StreamObserver<UpdateResponse> responseObserver) {
+    log.info("Updating `blocks` table");
 
-    // TODO: Implement this method
+    try {
+      // Fetch and populate `blocks` table
+      fetchAndPopulateHiveTable(blocksService, request);
+
+      // Return response
+      responseObserver.onNext(buildUpdateResponse());
+      responseObserver.onCompleted();
+    } catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+      responseObserver.onError(e.fillInStackTrace());
+    }
   }
 
   @Override
@@ -91,12 +101,36 @@ public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
       UpdateRequest request,
       StreamObserver<UpdateResponse> responseObserver
   ) {
-    // TODO: Implement this method
+    log.info("Updating `contracts` table");
+
+    try {
+      // Fetch and populate `contracts` table
+      fetchAndPopulateHiveTable(contractsService, request);
+
+      // Return response
+      responseObserver.onNext(buildUpdateResponse());
+      responseObserver.onCompleted();
+    } catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+      responseObserver.onError(e.fillInStackTrace());
+    }
   }
 
   @Override
   public void updateLogs(UpdateRequest request, StreamObserver<UpdateResponse> responseObserver) {
-    // TODO: Implement this method
+    log.info("Updating `logs` table");
+
+    try {
+      // Fetch and populate `logs` table
+      fetchAndPopulateHiveTable(logsService, request);
+
+      // Return response
+      responseObserver.onNext(buildUpdateResponse());
+      responseObserver.onCompleted();
+    } catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+      responseObserver.onError(e.fillInStackTrace());
+    }
   }
 
   @Override
@@ -104,17 +138,53 @@ public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
       UpdateRequest request,
       StreamObserver<UpdateResponse> responseObserver
   ) {
-    // TODO: Implement this method
+    log.info("Updating `token_transfers` table");
+
+    try {
+      // Fetch and populate `token_transfers` table
+      fetchAndPopulateHiveTable(tokenTransfersService, request);
+
+      // Return response
+      responseObserver.onNext(buildUpdateResponse());
+      responseObserver.onCompleted();
+    } catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+      responseObserver.onError(e.fillInStackTrace());
+    }
   }
 
   @Override
   public void updateTokens(UpdateRequest request, StreamObserver<UpdateResponse> responseObserver) {
-    // TODO: Implement this method
+    log.info("Updating `tokens` table");
+
+    try {
+      // Fetch and populate `tokens` table
+      fetchAndPopulateHiveTable(tokensService, request);
+
+      // Return response
+      responseObserver.onNext(buildUpdateResponse());
+      responseObserver.onCompleted();
+    } catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+      responseObserver.onError(e.fillInStackTrace());
+    }
   }
 
   @Override
   public void updateTraces(UpdateRequest request, StreamObserver<UpdateResponse> responseObserver) {
-    // TODO: Implement this method
+    log.info("Updating `traces` table");
+
+    try {
+      // Fetch and populate `traces` table
+      fetchAndPopulateHiveTable(tracesService, request);
+
+      // Return response
+      responseObserver.onNext(buildUpdateResponse());
+      responseObserver.onCompleted();
+    } catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+      responseObserver.onError(e.fillInStackTrace());
+    }
   }
 
   @Override
@@ -122,7 +192,19 @@ public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
       UpdateRequest request,
       StreamObserver<UpdateResponse> responseObserver
   ) {
-    // TODO: Implement this method
+    log.info("Updating `transactions` table");
+
+    try {
+      // Fetch and populate `transactions` table
+      fetchAndPopulateHiveTable(transactionsService, request);
+
+      // Return response
+      responseObserver.onNext(buildUpdateResponse());
+      responseObserver.onCompleted();
+    } catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+      responseObserver.onError(e.fillInStackTrace());
+    }
   }
 
   private void writeTableResults(
@@ -134,15 +216,6 @@ public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
         genericService.getStructStr(),
         tableResult
     );
-  }
-
-  private void writeTableResults(
-      String outputPath,
-      String fileName,
-      String struct,
-      TableResult tableResult
-  ) throws IOException {
-    OrcFileWriter.writeTableResults(outputPath + fileName, struct, tableResult);
   }
 
   private void createTmpTable(GenericService genericService) {
@@ -159,15 +232,6 @@ public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
     jdbcTemplate.execute(query);
   }
 
-  private void createTmpTable(String schema, String tmpTableName, String orcParentDir) {
-    // Use non-external table so that temporary file can be removed with table drop
-    String sql =
-        "CREATE TABLE %s (%s)"
-            + " STORED AS ORC LOCATION '%s' TBLPROPERTIES ('ORC.COMPRESS' = 'ZLIB')";
-    String query = String.format(sql, schema, tmpTableName, orcParentDir);
-    jdbcTemplate.execute(query);
-  }
-
   private void populateHiveTable(GenericService genericService) {
     String sql =
         "INSERT INTO ethernet.%s SELECT * FROM %s";
@@ -179,23 +243,30 @@ public class CoreServiceImpl extends CoreServiceGrpc.CoreServiceImplBase {
     jdbcTemplate.execute(query);
   }
 
-  private void populateHiveTable(String desTable, String srcTable) {
-    String sql =
-        "INSERT INTO ethernet.%s SELECT * FROM %s";
-    String query = String.format(sql, desTable, srcTable);
-    jdbcTemplate.execute(query);
-  }
-
   private void dropTmpTable(GenericService genericService) {
     String sql = "DROP TABLE %s";
     String query = String.format(sql, genericService.getTableName());
     jdbcTemplate.execute(query);
   }
 
-  private void dropTmpTable(String tmpTableName) {
-    String sql = "DROP TABLE %s";
-    String query = String.format(sql, tmpTableName);
-    jdbcTemplate.execute(query);
+  private void fetchAndPopulateHiveTable(
+      GenericService genericService,
+      UpdateRequest updateRequest
+  ) throws InterruptedException, IOException {
+    // Fetch results from BigQuery
+    TableResult tableResult = genericService.fetchFromBq(updateRequest);
+
+    // Write query results to ORC file with random (UUID) filename in HDFS
+    writeTableResults(genericService, tableResult);
+
+    // Create temporary Hive table
+    createTmpTable(genericService);
+
+    // Insert data from temporary Hive table
+    populateHiveTable(genericService);
+
+    // Remove temporary Hive table + temporary ORC file
+    dropTmpTable(genericService);
   }
 
 }
