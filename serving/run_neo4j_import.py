637f7c61b75153c7361eca07cf86dfa75ca0da47
suvoonhou <voonhousu@gmail.com>
2021-03-12 02:36:20 +0800

    Added Python sdk files

--- serving/run_neo4j_import.py
+++ serving/run_neo4j_import.py
@@ -15,6 +15,9 @@ def run_neo4j_import(host: str, port: str, container_name: str, command: str):
   std_out = conn.eval("os.popen(\"{}\").read()".format(docker_cmd_prefix + command))
   print(std_out)
 
+  # Close the rpyc connection
+  conn.close()
+
 if __name__ == "__main__":
   # First argument is the python file name at index 0
   host = sys.argv[1]
